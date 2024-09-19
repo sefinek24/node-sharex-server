e2b871f6b3d9439e3cbfc34f89ddbcb363c3cbb6
Sefinek <contact@sefinek.net>
2024-09-19 03:50:47 +0200

    Update morgan

--- middlewares/morgan.js
+++ middlewares/morgan.js
@@ -1,5 +1,3 @@
 const morgan = require('morgan');
 
-morgan.token('body', req => JSON.stringify(req.body));
-
-module.exports = morgan('[:status :method :response-time ms] :url :user-agent ":referrer" :body');
+module.exports = morgan('[:status :method :response-time ms] :url :user-agent :remote-addr ":referrer"');
